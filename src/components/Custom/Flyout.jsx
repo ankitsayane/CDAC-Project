894ce90343b67d40cba387583f39e6fa8b166638
ankitsayane <ankitsayane256@gmail.com>
2024-08-11 00:43:13 +0530

    update the Header and Flyout

--- src/components/Custom/Flyout.jsx
+++ src/components/Custom/Flyout.jsx
@@ -1,38 +1,51 @@
-import React from 'react'
+import React from "react";
 import { Button } from "../ui/button";
 import { Link, useNavigate } from "react-router-dom";
 function Flyout() {
-    const user = JSON.parse(sessionStorage.getItem("user"));
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        sessionStorage.removeItem("isLoggedIn");
-        navigate("/");
-        window.location.reload();
-      };
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.removeItem("isLoggedIn");
+    sessionStorage.removeItem("user");
+    navigate("/");
+    window.location.reload();
+  };
   return (
-    <div className='w-80 top-16 right-3 bg-zinc-100 absolute right-3 rounded-md overflow-hidden, border-2 , shadow-xl'>
-        <div className="flex justify-center mb-4 , py-2">
-        <img id='userprofile' src="https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1328.jpg?w=826" alt="" className='w-24 h-24 rounded-full object-cover m-1'/>
+    <div className="w-80 top-16 right-3 bg-zinc-100 absolute rounded-md overflow-hidden, border-2 , shadow-xl">
+      <div className="flex flex-col items-center mb-4 py-2">
+        <img
+          src="profile.svg"
+          className="w-24 h-24 rounded-full object-cover mb-2"
+        />
+        <h2 className="text-lg font-bold text-gray-900">{user.username}</h2>
       </div>
 
-        <div className='w-full px-3 py-1'>
-            <h2 className='font-semibold'>Name: {user.name}</h2>
+      <div className="w-full px-4 py-3">
+        <div className="flex flex-col space-y-3">
+          <div className="flex items-center">
+            <span className="w-16 font-semibold text-gray-700">Name</span>
+            <span className="text-gray-900">{user.name}</span>
+          </div>
+          <div className="flex items-center">
+            <span className="w-16 font-semibold text-gray-700">E-mail</span>
+            <span className="text-gray-900">{user.email}</span>
+          </div>
         </div>
-        <div className='w-full px-3 py-2'>
-            <h2 className='font-semibold'>Email: {user.email}</h2>
-        </div>
-        <div className='px-3 py-2'>
-          <Link to={"/travelplans/"+user.email}>
-            <Button className="w-full py-2 text-sm ,ml-2" >Trip History</Button>
-          </Link>
-        </div>
+      </div>
 
+      <div className="px-3 py-2">
+        <Link to={"/travelplans/" + user.email}>
+          <Button className="w-full py-2 text-sm ,ml-2">Trip History</Button>
+        </Link>
+      </div>
 
-        <div className='px-3 py-2'>
-        <Button onClick={handleLogout} className="w-full py-2 text-sm">Logout</Button>
+      <div className="px-3 py-2">
+        <Button onClick={handleLogout} className="w-full py-2 text-sm">
+          Logout
+        </Button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Flyout;
--- src/components/Custom/Header.jsx
+++ src/components/Custom/Header.jsx
@@ -1,19 +1,14 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "../ui/button";
 import { Link, useNavigate } from "react-router-dom";
 import Flyout from "./Flyout";
 
 function Header() {
   const isLoggedIn = !!sessionStorage.getItem("isLoggedIn");
-  const user = JSON.parse( sessionStorage.getItem("user"));
- 
+  const user = JSON.parse(sessionStorage.getItem("user"));
+
   const [isFlyoutOpen, setIsFlyoutOpen] = useState(false);
 
-  // const handleLogout = () => {
-  //   sessionStorage.removeItem("isLoggedIn");
-  //   navigate("/");
-  //   window.location.reload();
-  // };
   const toggleFlyout = () => {
     setIsFlyoutOpen(!isFlyoutOpen);
   };
@@ -29,34 +24,32 @@ function Header() {
     };
   }, []);
 
-  // const handleLogout = () => {
-  //   sessionStorage.removeItem("isLoggedIn");
-  //   sessionStorage.removeItem("user");
-  //   navigate("/");
-  //   window.location.reload();
-  // };
-
   return (
     <div>
       <div className="p-3 shadow-sm flex justify-between items-center px-5">
-      <img src="/compassly.png" alt="" className="w-1/5 bg-cover min-w-56" />
-
-      <div>
+        <Link to={"/"}>
+          <img
+            src="/compassly.png"
+            alt=""
+            className="w-1/5 bg-cover min-w-56"
+          />
+        </Link>
+        <div>
           {isLoggedIn ? (
             <>
-              <span className="mr-6 font-bold text-[#0039a6]">
-                Welcome, {user.name?user.name.charAt(0).toUpperCase() + user.name.slice(1):"user"}
+              <span className="mr-4 font-bold text- text-[#0039a6]">
+                Welcome,{" "}
+                {user.name
+                  ? user.name.charAt(0).toUpperCase() + user.name.slice(1)
+                  : "user"}
                 <img
                   id="profile"
-                  src="https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1328.jpg?w=826"
-                  alt=""
-                  className="size-9 bg-cover absolute right-0 top-4 mr-2"
+                  src="profile.svg"
+                  className="inline-block size-10 bg-cover ml-2 cursor-pointer"
                   onClick={toggleFlyout}
                 />
                 {isFlyoutOpen && <Flyout />}
               </span>
-
-              {/* <Button onClick={handleLogout}>Logout</Button> */}
             </>
           ) : (
             <>
